8227385bb80269fd5a8f5d8a0e0a049794ccac8b
Minori-Takeuchi <shato.min0716@gmeail.com>
2023-01-05 11:24:25 +0900

    first commit

---
+++ todoapp/app/Http/Controllers/TodoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $todos = Todo::select('created_at','name')->get();
+        return view('index',['todos' => $todos]);
+    }
+
+    public function create(TodoRequest $request)
+    {
+        $form = $request->all();
+        unset($form['_token']);
+        Todo::create($form);
+        return redirect('/');
+    }
+
+    public function update(TodoRequest $request)
+    {
+        $todo = Todo::find($request->id);
+        $form = $request->select('name');
+        unset($form['_token']);
+        Todo::where('name',$request->name)->update($form);
+        return redirect('/');
+    }
+
+    public function delete(Request $request)
+    {
+        $todo = Todo::find($request->id)->delete();
+        return redirect('/');
+    }
+}
---
+++ todoapp/resources/views/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+  <title>COACHTECH</title>
+</head>
+<body>
+  <div class="container">
+    <div class="card">
+      <p class="ttl">Todo List</p>
+      <div class="todo">
+        <form action="/create" method="post" class="frex">
+          @csrf
+          <input type="text" class="input-create" name="name">
+          <button class="btn-create">追加</button>
+        </form>
+        <table>
+          <tr>
+            <th>作成日</th>
+            <th>タスク名</th>
+            <th>更新</th>
+            <th>削除</th>
+          </tr>
+          @foreach($todos as $todo)
+          <tr>
+            <td>{{$todo->created_at}}</td>
+            <form action="/update" method="post">
+            @csrf
+              <td><input type="text" class="input-update" name="new-name" value="{{$todo->name}}"></td>
+              <td><button class="btn-update">更新</button></td>
+            </form>
+            <form action="/delete" method="post">
+            @csrf
+              <td><button class="btn-delete">削除</button></td>
+            </form>
+            </tr>
+            @endforeach
+          </table>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
